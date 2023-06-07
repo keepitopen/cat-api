c01019f51e837c879605d2ac2bd316b64d8eb17a
Akshat Gupta <akshat.gupta@tricog.com>
2023-06-07 15:57:05 +0530

    edit .gitignore

--- src/app/config/constants.ts
+++ src/app/config/constants.ts
@@ -1,4 +1,4 @@
 import path from 'path';
 
-export const TEMP_DIR = path.join(process.cwd(), '/uploads');
+export const TEMP_DIR = path.join(process.cwd(), '/tmp');
 export const UPLOADS_DIR = path.join(process.cwd(), '/uploads');
